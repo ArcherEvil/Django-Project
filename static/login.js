c8472bebb6a7d62cf35268d386068f1253c0f592
Archer <archerevil2005@gmail.com>
2022-01-13 18:39:16 +0000

    Html Introduced

---
+++ static/login.js
@@ -0,0 +1,10 @@
+let form = document.querySelector('#form');
+    
+form.addEventListener('submit', function(foda) {
+    let username = document.querySelector('.user').value
+    let password = document.querySelector('.pass').value
+        
+        
+})
+
+
---
+++ static/main.js
@@ -0,0 +1,7 @@
+function login() {
+    window.location.href = '../login/';
+}
+
+function register() {
+    window.location.href = '../register/';
+}
